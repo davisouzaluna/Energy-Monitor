12a936042590c29cd9c7497eb20e7622281d1a04
Thaisvasconcelos49 <vasconcelosthais49@gmail.com>
2023-04-08 14:27:15 -0300

    Criação dos scripts de inserção e seleção de dados.

---
+++ Energy_Monitor.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE energy_monitor;
+USE energy_monitor;
+
+CREATE TABLE consumo (
+  id INT NOT NULL,
+  aparelho VARCHAR(50),
+  corrente INT
+);
+
+INSERT INTO consumo (id, aparelho, corrente)
+VALUES
+    (1, 'geladeira', 10),
+    (2, 'ar condicionado', 17),
+    (3, 'sanduicheira', 2);
+
+SELECT * FROM consumo;
+
