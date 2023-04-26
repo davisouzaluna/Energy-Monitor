59e828b1351fd7b6e72efce1ea4d415d45718be1
Gabriel Vidal <gabrielvidal1590@gmail.com>
2023-04-26 18:22:22 -0300

    criação da tabela logs_erro

--- BD/Energy_Monitor.sql
+++ BD/Energy_Monitor.sql
@@ -11,6 +11,13 @@ CREATE TABLE consumo (
   CONSTRAINT PK_consumo PRIMARY KEY (id)
 );
 
+CREATE TABLE logs_erro (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  datahora DATETIME NOT NULL DEFAULT,
+  tipo VARCHAR(50) NOT NULL,
+  mensagem TEXT NOT NULL
+)
+
 /* EXEMPLOS DE OPERAÇÕES ENVOLVENDO O BD
 INSERT INTO consumo (aparelho, corrente,qos)
 VALUES
