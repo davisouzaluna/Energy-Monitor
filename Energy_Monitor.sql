05b3a60a886ba8e8d53ddd66c87f45aec228bd4d
Gabriel Vidal <gabrielvidal1590@gmail.com>
2023-04-26 17:51:49 -0300

    atualizando meu banco com base no principal

--- Energy_Monitor.sql
+++ Energy_Monitor.sql
@@ -1,8 +1,20 @@
+# Banco de dados para armazenar dados da corrente dos dispositivos do projeto energy_monitor
+
 CREATE DATABASE energy_monitor;
 USE energy_monitor;
 
 CREATE TABLE consumo (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   aparelho VARCHAR(50),
-  corrente INT
+  qos INT NOT NULL,
+  corrente INT NOT NULL,
+  CONSTRAINT PK_consumo PRIMARY KEY (id)
 );
+
+/* EXEMPLOS DE OPERAÇÕES ENVOLVENDO O BD
+INSERT INTO consumo (aparelho, corrente,qos)
+VALUES
+    ('geladeira', 10, 0),
+    ('ar condicionado', 17, 0),
+    ('sanduicheira', 2, 0);
+SELECT * FROM consumo;
