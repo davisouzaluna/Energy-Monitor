94c0029f802b1c1f54843581915dc56d44b39d51
Danpam12 <dpbm@discente.ifpe.edu.br>
2023-06-16 01:13:44 -0300

    Estilização do aplicativo – em desenvolvimeto.
    Co-authored-by: Thaisvasconcelos49 <vascocelosthais49@gmail.com>
    Co-authored-by: ivan-teotonio <ivanteotonio256@gmail.com>

--- aplicacao/resources/views/dashboard.blade.php
+++ aplicacao/resources/views/dashboard.blade.php
@@ -6,19 +6,60 @@
   </x-slot>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   
-      <div class="py-12">
+  <div class="py-1">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900 text-center"> <!-- adicionado "text-center" aqui -->
-          {{ __("Bem-vindo(a) ao Sistema Monitoramento de Corrente") }} <br><br>
-  
-          {{ __("Para visualizar o gráfico com os últimos 10 valores medidos, clique no botão abaixo: ")}} <br><br>
-          <a href="{{ route('sensor.ultimos-dez') }}" class="inline-block px-6 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition-colors">Visualizar Gráfico</a>
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-4 sm:p-6 mt-1 mb-1"> <!-- Ajustando as margens superior (mt-4) e inferior (mb-2) aqui -->
+                <h2 class="text-lg font-semibold mb-4">{{ __("Cadastre um novo dispositivo:") }}</h2>
+
+                <form method="post" action="{{ route('device.store') }}" class="space-y-6">
+                    @csrf
+                    @method('put')
+
+                    <div>
+                        <label for="nome" class="mb-1 block font-semibold text-gray-700">{{ __('Nome') }}</label>
+                        <input id="nome" name="nome" type="text" class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="nome" />
+                    </div>
+
+                    <div>
+                        <label for="descricao" class="mb-1 block font-semibold text-gray-700">{{ __('Descrição') }}</label>
+                        <input id="descricao" name="descricao" type="text" class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="descricao" />
+                    </div>
+
+                    <div>
+                        <label for="mac" class="mb-1 block font-semibold text-gray-700">{{ __('MAC') }}</label>
+                        <input id="mac" name="mac" type="text" class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="mac" />
+                    </div>
+
+                    <div class="flex justify-end">
+                        <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-md">{{ __('Salvar') }}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+         {{-- <a href="{{ route('sensor.ultimos-dez') }}" class="inline-block px-6 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition-colors">Visualizar Gráfico</a>
           <a href="{{ route('device.index') }}" class="inline-block px-6 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition-colors">Cadastrar dispositivo</a>
           <a href="{{ route('log.erro') }}" class="inline-block px-6 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition-colors">Log dos erros</a>
-  
+         --}}
           <br><br>
           <h3>Dispositivos cadastrados:</h3>
+         <div class="grid grid-cols-3 gap-4">
+    <a href="pagina1.html" class="bg-white shadow rounded-lg">
+        <img src="caminho/para/imagem1.jpg" alt="Imagem 1" class="w-full h-full object-cover">
+    </a>
+    <a href="pagina2.html" class="bg-white shadow rounded-lg">
+        <img src="caminho/para/imagem2.jpg" alt="Imagem 2" class="w-full h-full object-cover">
+    </a>
+    <a href="pagina3.html" class="bg-white shadow rounded-lg">
+        <img src="caminho/para/imagem3.jpg" alt="Imagem 3" class="w-full h-full object-cover">
+    </a>
+            <!-- Adicione mais cards de imagens aqui, se necessário -->
+        </div>
+        
+        
           <table class="table">
               <thead>
                   
