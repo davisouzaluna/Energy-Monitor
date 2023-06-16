00f2657b1903b6665a7ec70edfe4a4a2dc2f8e00
Davi Souza de Luna <Sdavi738@gmail.com>
2023-06-16 07:56:31 -0300

    Consertando erro de rota do formulario do dashboard

--- aplicacao/resources/views/dashboard.blade.php
+++ aplicacao/resources/views/dashboard.blade.php
@@ -10,30 +10,27 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-4 sm:p-6 mt-1 mb-1"> <!-- Ajustando as margens superior (mt-4) e inferior (mb-2) aqui -->
-                <h2 class="text-lg font-semibold mb-4">{{ __("Cadastre um novo dispositivo:") }}</h2>
-
-                <form method="post" action="{{ route('device.store') }}" class="space-y-6">
+                
+                <form method="POST" action="{{ route('device.store') }}">
                     @csrf
-                    @method('put')
 
                     <div>
-                        <label for="nome" class="mb-1 block font-semibold text-gray-700">{{ __('Nome') }}</label>
-                        <input id="nome" name="nome" type="text" class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="nome" />
-                    </div>
-
-                    <div>
-                        <label for="descricao" class="mb-1 block font-semibold text-gray-700">{{ __('Descrição') }}</label>
-                        <input id="descricao" name="descricao" type="text" class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="descricao" />
+                        <label for="nome">Nome:</label>
+                        <input type="text" name="nome" id="nome" required value="{{ old('nome') }}" >
                     </div>
 
                     <div>
-                        <label for="mac" class="mb-1 block font-semibold text-gray-700">{{ __('MAC') }}</label>
-                        <input id="mac" name="mac" type="text" class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="mac" />
+                        <label for="descricao">Descrição:</label>
+                        <input type="text" name="descricao" id="descricao" required value="{{ old('descricao') }}" >
                     </div>
 
-                    <div class="flex justify-end">
-                        <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-md">{{ __('Salvar') }}</button>
+                    <div>
+                        <label for="mac">MAC:</label>
+                        <input type="text" name="MAC" id="mac" required value="{{ old('MAC') }}" >
+                        {{-- <small>O MAC deve conter exatamente 12 caracteres alfanuméricos (A-F, a-f, 0-9).</small> --}}
                     </div>
+
+                    <button type="submit">Enviar</button>
                 </form>
             </div>
         </div>
