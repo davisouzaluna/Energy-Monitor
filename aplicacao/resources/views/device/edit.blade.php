e93241b20cddd272493a77746c71b4e1ed69058a
Davi Souza de Luna <Sdavi738@gmail.com>
2023-06-23 06:35:25 -0300

    feat:Relatório de consumo em kwh

--- aplicacao/resources/views/device/edit.blade.php
+++ aplicacao/resources/views/device/edit.blade.php
@@ -1,6 +1,3 @@
-@extends('layouts.ppa')
-
-
 <x-app-layout>
 
     <style>
@@ -19,56 +16,75 @@
         }
     </style>
 
+<div class="py-1 flex justify-center">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-2 gap-4">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-4 sm:p-6 mt-1 mb-1">
+                <!-- Ajustando as margens superior (mt-4) e inferior (mb-2) aqui -->
+                <h2 class="text-lg font-semibold mb-4">{{ __('Altere o seu dispositivo:') }}</h2>
 
+                <div div class="py-1 flex justify-center mb-4">
+                    <form method="POST" action="{{ route('device.update', $dispositivo->id) }}"
+                        class="space-y-6">
+                        @csrf
+                        @method('PUT')
 
-    <div class="py-1 flex justify-center">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-4 sm:p-6 mt-1 mb-1">
-                    <!-- Ajustando as margens superior (mt-4) e inferior (mb-2) aqui -->
-                    <h2 class="text-lg font-semibold mb-4">{{ __('Altere o seu dispositivo:') }}</h2>
+                        <div>
+                            <label for="nome" class="mb-1 block font-semibold text-gray-700">Nome:</label>
+                            <input type="text" name="nome" id="nome" required
+                                value="{{ old('nome') ?? $dispositivo->nome }}"
+                                class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="nome">
+                        </div>
 
-                    <div div class="py-1 flex justify-center mb-4">
-                        <form method="POST" action="{{ route('device.update', $dispositivo->id) }}" class="space-y-6">
-                            @csrf
-                            @method('PUT')
+                        <div>
+                            <label for="descricao"
+                                class="mb-1 block font-semibold text-gray-700">Descrição:</label>
+                            <input type="text" name="descricao" id="descricao" required
+                                value="{{ old('descricao') ?? $dispositivo->descricao }}"
+                                class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="descricao">
+                        </div>
 
-                            <div>
-                                <label for="nome" class="mb-1 block font-semibold text-gray-700">Nome:</label>
-                                <input type="text" name="nome" id="nome" required
-                                    value="{{ old('nome') ?? $dispositivo->nome }}"
-                                    class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="nome">
-                            </div>
+                        <div>
+                            <label for="mac" class="mb-1 block font-semibold text-gray-700">MAC:</label>
+                            <input type="text" name="MAC" maxlength="12" id="mac" required
+                                value="{{ old('MAC') ?? $dispositivo->MAC }}"
+                                class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="mac">
+                            {{-- <small>O MAC deve conter exatamente 12 caracteres alfanuméricos (A-F, a-f, 0-9).</small> --}}
+                        </div>
 
-                            <div>
-                                <label for="descricao" class="mb-1 block font-semibold text-gray-700">Descrição:</label>
-                                <input type="text" name="descricao" id="descricao" required
-                                    value="{{ old('descricao') ?? $dispositivo->descricao }}"
-                                    class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="descricao">
-                            </div>
-
-                            <div>
-                                <label for="mac" class="mb-1 block font-semibold text-gray-700">MAC:</label>
-                                <input type="text" name="MAC" maxlength="12" id="mac" required
-                                    value="{{ old('MAC') ?? $dispositivo->MAC }}"
-                                    class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="mac">
-                                {{-- <small>O MAC deve conter exatamente 12 caracteres alfanuméricos (A-F, a-f, 0-9).</small> --}}
-                            </div>
-
-                            <div class="flex justify-center">
-                                <button type="submit"
-                                    class="px-4 py-2  bg-blue-500 text-white rounded-md transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200 ">Alterar</button>
-                            </div>
-                        </form>
-                    </div>
+                        <div class="flex justify-center">
+                            <button type="submit"
+                                class="px-4 py-2  bg-blue-500 text-white rounded-md transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200 ">Alterar</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
+
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-4 sm:p-6 mt-1 mb-1">
+                <!-- Ajustando as margens superior (mt-4) e inferior (mb-2) aqui -->
+                <h2 class="text-lg font-semibold mb-4">{{ __('Relatório de consumo:') }}</h2>
+                <div class="py-1 flex justify-center">
+                    <div id="kwh" class="mt-4 text-gray-700"></div>
+                </div>
+            
+                <!-- Conteúdo da segunda div -->
+            </div>
+        </div>
     </div>
+</div>
 
 
 
-
+    <div class="py-1 justify center">
+        <div class="flex flex-col items-end mt-4">
+            <label for="voltageSelect" class="text-gray-700">Selecione a tensão:</label>
+            <select id="voltageSelect" class="mt-2">
+                <option value="110">110V</option>
+                <option value="220">220V</option>
+            </select>
+        </div>
 
     <div class="py-1 flex justify-center items-center mb-4">
         <div class="flex flex-col items-end mt-4">
@@ -84,27 +100,39 @@
             </select>
         </div>
 
+        
+
+        <div id="chart-container" class="chart-container">
+            <canvas id="myChart" class="chart-canvas"></canvas>
+        </div>
+    </div>
+    
+
+    <div class="py-1 flex justify-center">
+        <form action="{{ route('device.destroy', $dispositivo->id) }}" method="post">
+            @csrf
+            @method('delete')
+            <button type="submit"
+                class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded">Excluir
+                dispositivo</button>
+        </form>
+    </div>
+
         <div id="chart-container" class="chart-container">
             <canvas id="myChart" class="chart-canvas"></canvas>
         </div>
     </div>
 
-
-
-
-
-
-
-
-
+    
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             const mac = "{{ $dispositivo->MAC }}";
             let range = "{{ 'day' }}"; // Valor padrão
             let myChart = null; // Variável para armazenar a instância do gráfico
+            let soma = 0; // Variável para armazenar a soma dos valores
 
-            // Escuta o evento de mudança no elemento <select>
+            // Escuta o evento de mudança no elemento <select> (intervalo)
             document.getElementById('rangeSelect').addEventListener('change', function() {
                 range = this.value; // Atualiza a variável range com o valor selecionado
                 fetchData(mac, range); // Chama a função para buscar os dados com o novo range
@@ -117,6 +145,9 @@
                     .then(data => {
                         const labels = data.labels;
                         const valores = data.valores;
+
+                        // Calcula a soma dos valores
+                        soma = valores.reduce((a, b) => a + b, 0);
 
                         if (myChart) {
                             myChart.destroy(); // Destroi a instância atual do gráfico
@@ -156,7 +187,7 @@
                                 plugins: {
                                     title: {
                                         display: true,
-                                        text: 'Gráfico de Medição', // Título do gráfico
+                                        text: 'Gráfico do consumo em Amperes', // Título do gráfico
                                         font: {
                                             size: 18
                                         }
@@ -164,33 +195,37 @@
                                 }
                             }
                         });
+
+                        // Atualiza o valor em kWh
+                        updateKwhValue();
                     })
                     .catch(error => {
                         console.error('Ocorreu um erro:', error);
                     });
             }
 
+            // Função para atualizar o valor em kWh
+            function updateKwhValue() {
+                const voltageSelect = document.getElementById('voltageSelect');
+                const voltage = voltageSelect.value;
+                const kwh = (soma * voltage * 0.001).toFixed(2); // Cálculo em kWh
+
+                document.getElementById('kwh').textContent = `${kwh} kWh`;
+            }
+
+            // Escuta o evento de mudança no elemento <select> (tensão)
+            document.getElementById('voltageSelect').addEventListener('change', function() {
+                updateKwhValue(); // Atualiza o valor em kWh
+            });
+
             // Chama a função para buscar os dados iniciais com o range padrão
             fetchData(mac, range);
 
-            // Atualiza o gráfico a cada minuto
+            // Atualiza o gráfico e o valor em kWh a cada minuto
             setInterval(function() {
                 fetchData(mac, range);
             }, 60000); // 60000 milissegundos = 1 minuto
         });
     </script>
-
-
-
-    <div div class="py-1 flex justify-center">
-        <form action="{{ route('device.destroy', $dispositivo->id) }}" method="post">
-            @csrf
-            @method('delete')
-            <button type="submit"
-                class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded">Excluir
-                dispositivo</button>
-        </form>
-    </div>
-
 
 </x-app-layout>
