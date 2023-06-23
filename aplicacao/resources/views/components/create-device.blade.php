d8989baf46fdc08e109a78c206d45aed538717e0
Davi Souza de Luna <Sdavi738@gmail.com>
2023-06-23 05:33:56 -0300

    feat:Adicionando modal e animação

--- aplicacao/resources/views/components/create-device.blade.php
+++ aplicacao/resources/views/components/create-device.blade.php
@@ -1,39 +1,115 @@
-<div class="py-1 flex justify-center">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-4 sm:p-6 mt-1 mb-1">
-                <!-- Ajustando as margens superior (mt-4) e inferior (mb-2) aqui -->
-                <h2 class="text-lg font-semibold mb-4">{{ __('Cadastre um novo dispositivo:') }}</h2>
-                <form method="POST" action="{{ route('device.store') }}" class="space-y-6">
-                    @csrf
+<!-- Botão para abrir o modal -->
+<button onclick="openModal()" class="px-4 py-2 bg-blue-500 text-white rounded-md transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200">Abrir Modal</button>
 
-                    <div>
-                        <label for="nome" class="mb-1 block font-semibold text-gray-700">Nome:</label>
-                        <input type="text" name="nome" id="nome" required value="{{ old('nome') }}"
-                            class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="nome">
-                    </div>
-
-                    <div>
-                        <label for="descricao" class="mb-1 block font-semibold text-gray-700">Descrição:</label>
-                        <input type="text" name="descricao" id="descricao" required
-                            value="{{ old('descricao') }}" class="px-2 py-1 block w-full border-gray-300 rounded-md"
-                            autocomplete="descricao">
-                    </div>
-
-                    <div>
-                        <label for="mac" class="mb-1 block font-semibold text-gray-700">MAC:</label>
-                        <input type="text" maxlength="12" name="MAC" id="mac" required value="{{ old('MAC') }}"
-                            class="px-2 py-1 block w-full border-gray-300 rounded-md" autocomplete="mac">
-                        {{-- <small>O MAC deve conter exatamente 12 caracteres alfanuméricos (A-F, a-f, 0-9).</small> --}}
-                    </div>
-
-                    <div class="flex justify-center ">
-                        <button type="submit" class="px-4 py-2  bg-blue-500 text-white rounded-md transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200 ">Enviar</button>
-                        
-                    </div>
-
-                </form>
-            </div>
+<!-- Modal -->
+<div id="myModal" class="modal-overlay">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h2 class="text-xl font-medium text-gray-900 dark:text-white">{{ __('Cadastre um novo dispositivo:') }}</h2>
+            <button type="button" class="close" onclick="closeModal()">&times;</button>
+        </div>
+        <div class="modal-body">
+            <form method="POST" action="{{ route('device.store') }}" class="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8">
+                @csrf
+                <div class="form-group">
+                    <label for="nome" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Nome:</label>
+                    <input type="text" name="nome" id="nome" required value="{{ old('nome') }}" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" autocomplete="nome">
+                </div>
+                <div class="form-group">
+                    <label for="descricao" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Descrição:</label>
+                    <input type="text" name="descricao" id="descricao" required value="{{ old('descricao') }}" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" autocomplete="descricao">
+                </div>
+                <div class="form-group">
+                    <label for="mac" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">MAC:</label>
+                    <input type="text" maxlength="12" name="MAC" id="mac" required value="{{ old('MAC') }}" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" autocomplete="mac">
+                </div>
+                <div class="flex justify-center">
+                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Enviar</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
+
+<!-- Script para abrir e fechar o modal -->
+<script>
+    function openModal() {
+        var modal = document.getElementById("myModal");
+        modal.style.display = "flex";
+        modal.classList.add("fade-in");
+    }
+
+    function closeModal() {
+        var modal = document.getElementById("myModal");
+        modal.classList.remove("fade-in");
+        setTimeout(function() {
+            modal.style.display = "none";
+        }, 300); // Tempo para a animação ser concluída
+    }
+
+    // Fechar o modal ao clicar fora dele
+    window.onclick = function(event) {
+        var modal = document.getElementById("myModal");
+        if (event.target === modal) {
+            closeModal();
+        }
+    };
+</script>
+
+<style>
+    .modal-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: none;
+        justify-content: center;
+        align-items: center;
+        z-index: 9999;
+    }
+
+    .modal-content {
+        background-color: #fff;
+        border-radius: 0.5rem;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+        max-width: 95%;
+        width: 30rem;
+        opacity: 0;
+        transform: translateY(-20px);
+        transition: opacity 0.3s ease, transform 0.3s ease;
+    }
+
+    .modal-overlay.fade-in .modal-content {
+        opacity: 1;
+        transform: translateY(0);
+    }
+
+    .modal-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+        border-bottom: 1px solid #ddd;
+    }
+
+    .modal-body {
+        padding: 1rem;
+    }
+
+    .modal-footer {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        padding: 1rem;
+        border-top: 1px solid #ddd;
+    }
+
+    .close {
+        background: none;
+        border: none;
+        cursor: pointer;
+        font-size: 1.5rem;
+    }
+</style>
